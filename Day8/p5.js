0dc3c98ef3c424eeffc80a2d1a8d7e26775f7418
faisal-2206 <faisalsiddiqui2206@gmail.com>
2024-10-21 15:45:02 +0530

    Updated

---
+++ Day8/p5.js
@@ -0,0 +1,15 @@
+async function orderFood(name,time){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(name +" Prepared")
+        }, time);
+    });
+}
+async function resturant() {
+    console.log("Task Started");
+    const  pizza = await orderFood("Pizza", 1000);
+    console.log(pizza);
+    const burger =await orderFood("Burger",2000);
+    console.log(burger);
+}
+resturant();
