946a4417bbef92ab42837295f80afba02336fc82
faisal-2206 <faisalsiddiqui2206@gmail.com>
2024-12-02 13:05:37 +0530

    New

---
+++ Day11/netflix-clone/src/MovieList.jsx
@@ -0,0 +1,19 @@
+import "./MovieList.css";
+import movies from "./movies";
+
+
+export const MovieList = () => {
+  return (
+    <div className="movie-list">
+        {movies.map((movie)=>
+        (<div key={movie.id} className="movie-card">
+            <img src={movie.image}/>
+            <h1></h1>
+            <h2></h2>
+            <h2></h2>
+        </div>
+    ))}
+    </div>
+  )
+}
+export default MovieList;
